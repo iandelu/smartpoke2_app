3fa90a37ccfecd79bae3df47adaf421137bba092
Anedell <48674592+iandelu@users.noreply.github.com>
2024-08-21 19:41:14 +0200

    product search

---
+++ lib/features/product/providers/product_provider.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'product_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$productProviderHash() => r'489a767b1c28b35c05fb55ea2ff9fb2659855828';
+
+/// See also [ProductProvider].
+@ProviderFor(ProductProvider)
+final productProviderProvider = AutoDisposeAsyncNotifierProvider<
+    ProductProvider, List<ProductModel>>.internal(
+  ProductProvider.new,
+  name: r'productProviderProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$productProviderHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$ProductProvider = AutoDisposeAsyncNotifier<List<ProductModel>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
